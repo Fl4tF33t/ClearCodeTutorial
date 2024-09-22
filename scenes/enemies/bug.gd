bec8b69ce9ea691a4628349eefb0b161e92543ba
Fl4tF33t <mhelmy.h97@gmail.com>
2024-09-22 19:04:53 +0300

    save

--- scenes/enemies/bug.gd
+++ scenes/enemies/bug.gd
@@ -5,8 +5,20 @@ var speed:= 300
 var can_be_damaged:= true
 var player_nearby:= false
 var direction: Vector2
+var health:= 20
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var attack_timer: Timer = $Timers/AttackTimer
+@onready var hit_timer: Timer = $Timers/HitTimer
+
+func hit():
+	if can_be_damaged:
+		can_be_damaged = false
+		hit_timer.start()
+		health -= 10
+		animated_sprite_2d.material.set_shader_parameter("progress", 0.8)
+	if health <= 0:
+		queue_free()
 
 func _process(_delta: float) -> void:
 	direction = (Globals.player_position - position).normalized()
@@ -27,7 +39,14 @@ func _on_notice_area_2d_body_exited(_body: Node2D) -> void:
 	active = false
 	animated_sprite_2d.stop()
 
-
 func _on_animated_sprite_2d_animation_finished() -> void:
 	if player_nearby:
 		Globals.health -= 10
+		attack_timer.start()
+
+func _on_attack_timer_timeout() -> void:
+	animated_sprite_2d.play("attack")
+	
+func _on_hit_timer_timeout() -> void:
+	can_be_damaged = true
+	animated_sprite_2d.material.set_shader_parameter("progress", 0)
